422636c947e3d2bf4a7a1055d3cdfb151e160bb8
Jacky Nguyen <nktpro@gmail.com>
2020-03-04 23:42:20 -0500

    Add all missing workload metrics

--- models/workload.go
+++ models/workload.go
@@ -7,74 +7,227 @@ import (
 )
 
 var (
-	workloadOperationHZ = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+	workloadBytesHZ = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "fdb_workload_bytes_per_second",
+		Help: "Workload bytes per second",
+	}, []string{"operation"})
+
+	workloadBytesCounter = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "fdb_workload_bytes_total",
+		Help: "Workload bytes total",
+	}, []string{"operation"})
+
+	workloadBytesRoughness = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "fdb_workload_bytes_roughness",
+		Help: "Workload bytes roughness",
+	}, []string{"operation"})
+
+	workloadKeysHZ = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "fdb_workload_keys_per_second",
+		Help: "Workload keys per second",
+	}, []string{"operation"})
+
+	workloadKeysCounter = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "fdb_workload_keys_total",
+		Help: "Workload keys total",
+	}, []string{"operation"})
+
+	workloadKeysRoughness = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "fdb_workload_keys_roughness",
+		Help: "Workload keys roughness",
+	}, []string{"operation"})
+	
+	workloadOperationsHZ = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "fdb_workload_operations_per_second",
-		Help: "operations per second",
+		Help: "Workload operations per second",
 	}, []string{"operation"})
 
-	workloadOperationCounter = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+	workloadOperationsCounter = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "fdb_workload_operations_total",
-		Help: "Gauge of operations",
+		Help: "Workload operations total",
 	}, []string{"operation"})
 
-	workloadTransactionHZ = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+	workloadOperationsRoughness = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "fdb_workload_operations_roughness",
+		Help: "Workload operations roughness",
+	}, []string{"operation"})
+
+	workloadTransactionsHZ = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "fdb_workload_transactions_per_second",
-		Help: "rate of transactions",
-	}, []string{"status"})
-	workloadTransactionCounter = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Help: "Workload transactions per second",
+	}, []string{"transaction"})
+
+	workloadTransactionsCounter = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "fdb_workload_transactions_total",
-		Help: "Gauge of operations",
-	}, []string{"status"})
+		Help: "Workload transactions total",
+	}, []string{"transaction"})
+
+	workloadTransactionsRoughness = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "fdb_workload_transactions_roughness",
+		Help: "Workload transactions roughness",
+	}, []string{"transaction"})
 )
 
 // ExportWorkload is exporting workloads
 func (s FDBStatus) ExportWorkload() {
+	workloadBytesHZ.With(prometheus.Labels{
+		"operation": "written",
+	}).Set(s.Cluster.Workload.Bytes.Written.Hz)
 
-	// writes
-	workloadOperationHZ.With(prometheus.Labels{
-		"operation": "writes",
-	}).Set(s.Cluster.Workload.Operations.Reads.Hz)
-	workloadOperationHZ.With(prometheus.Labels{
+	workloadBytesCounter.With(prometheus.Labels{
+		"operation": "written",
+	}).Set(s.Cluster.Workload.Bytes.Written.Counter)
+
+	workloadBytesRoughness.With(prometheus.Labels{
+		"operation": "written",
+	}).Set(s.Cluster.Workload.Bytes.Written.Roughness)
+
+	workloadBytesHZ.With(prometheus.Labels{
+		"operation": "read",
+	}).Set(s.Cluster.Workload.Bytes.Read.Hz)
+
+	workloadBytesCounter.With(prometheus.Labels{
+		"operation": "read",
+	}).Set(s.Cluster.Workload.Bytes.Read.Counter)
+
+	workloadBytesRoughness.With(prometheus.Labels{
+		"operation": "read",
+	}).Set(s.Cluster.Workload.Bytes.Read.Roughness)
+
+	workloadKeysHZ.With(prometheus.Labels{
+		"operation": "read",
+	}).Set(s.Cluster.Workload.Keys.Read.Hz)
+
+	workloadKeysCounter.With(prometheus.Labels{
+		"operation": "read",
+	}).Set(s.Cluster.Workload.Keys.Read.Counter)
+
+	workloadKeysRoughness.With(prometheus.Labels{
+		"operation": "read",
+	}).Set(s.Cluster.Workload.Keys.Read.Roughness)
+
+	workloadOperationsHZ.With(prometheus.Labels{
+		"operation": "read_requests",
+	}).Set(s.Cluster.Workload.Operations.ReadRequests.Hz)
+
+	workloadOperationsCounter.With(prometheus.Labels{
+		"operation": "read_requests",
+	}).Set(s.Cluster.Workload.Operations.ReadRequests.Counter)
+
+	workloadOperationsRoughness.With(prometheus.Labels{
+		"operation": "read_requests",
+	}).Set(s.Cluster.Workload.Operations.ReadRequests.Roughness)
+
+	workloadOperationsHZ.With(prometheus.Labels{
 		"operation": "reads",
+	}).Set(s.Cluster.Workload.Operations.Reads.Hz)
+
+	workloadOperationsCounter.With(prometheus.Labels{
+		"operation": "reads",
+	}).Set(s.Cluster.Workload.Operations.Reads.Counter)
+
+	workloadOperationsRoughness.With(prometheus.Labels{
+		"operation": "reads",
+	}).Set(s.Cluster.Workload.Operations.Reads.Roughness)
+
+	workloadOperationsHZ.With(prometheus.Labels{
+		"operation": "writes",
 	}).Set(s.Cluster.Workload.Operations.Writes.Hz)
 
-	// reads
-	workloadOperationCounter.With(prometheus.Labels{
+	workloadOperationsCounter.With(prometheus.Labels{
 		"operation": "writes",
-	}).Set(s.Cluster.Workload.Operations.Reads.Counter)
-	workloadOperationCounter.With(prometheus.Labels{
-		"operation": "reads",
 	}).Set(s.Cluster.Workload.Operations.Writes.Counter)
 
-	// commited transactions
-	workloadTransactionHZ.With(prometheus.Labels{
-		"status": "committed",
+	workloadOperationsRoughness.With(prometheus.Labels{
+		"operation": "writes",
+	}).Set(s.Cluster.Workload.Operations.Writes.Roughness)
+
+	workloadTransactionsHZ.With(prometheus.Labels{
+		"operation": "committed",
 	}).Set(s.Cluster.Workload.Transactions.Committed.Hz)
-	workloadTransactionCounter.With(prometheus.Labels{
-		"status": "committed",
+
+	workloadTransactionsCounter.With(prometheus.Labels{
+		"operation": "committed",
 	}).Set(s.Cluster.Workload.Transactions.Committed.Counter)
 
-	// conflicted transactions
-	workloadTransactionHZ.With(prometheus.Labels{
-		"status": "conflicted",
+	workloadTransactionsRoughness.With(prometheus.Labels{
+		"operation": "committed",
+	}).Set(s.Cluster.Workload.Transactions.Committed.Roughness)
+
+	workloadTransactionsHZ.With(prometheus.Labels{
+		"operation": "conflicted",
 	}).Set(s.Cluster.Workload.Transactions.Conflicted.Hz)
-	workloadTransactionCounter.With(prometheus.Labels{
-		"status": "conflicted",
+
+	workloadTransactionsCounter.With(prometheus.Labels{
+		"operation": "conflicted",
 	}).Set(s.Cluster.Workload.Transactions.Conflicted.Counter)
 
-	// started transactions
-	workloadTransactionHZ.With(prometheus.Labels{
-		"status": "started",
+	workloadTransactionsRoughness.With(prometheus.Labels{
+		"operation": "conflicted",
+	}).Set(s.Cluster.Workload.Transactions.Conflicted.Roughness)
+
+	workloadTransactionsHZ.With(prometheus.Labels{
+		"operation": "started",
 	}).Set(s.Cluster.Workload.Transactions.Started.Hz)
-	workloadTransactionCounter.With(prometheus.Labels{
-		"status": "started",
+
+	workloadTransactionsCounter.With(prometheus.Labels{
+		"operation": "started",
 	}).Set(s.Cluster.Workload.Transactions.Started.Counter)
+
+	workloadTransactionsRoughness.With(prometheus.Labels{
+		"operation": "started",
+	}).Set(s.Cluster.Workload.Transactions.Started.Roughness)
+
+	workloadTransactionsHZ.With(prometheus.Labels{
+		"operation": "started_batch_priority",
+	}).Set(s.Cluster.Workload.Transactions.StartedBatchPriority.Hz)
+
+	workloadTransactionsCounter.With(prometheus.Labels{
+		"operation": "started_batch_priority",
+	}).Set(s.Cluster.Workload.Transactions.StartedBatchPriority.Counter)
+
+	workloadTransactionsRoughness.With(prometheus.Labels{
+		"operation": "started_batch_priority",
+	}).Set(s.Cluster.Workload.Transactions.StartedBatchPriority.Roughness)
+
+	workloadTransactionsHZ.With(prometheus.Labels{
+		"operation": "started_default_priority",
+	}).Set(s.Cluster.Workload.Transactions.StartedDefaultPriority.Hz)
+
+	workloadTransactionsCounter.With(prometheus.Labels{
+		"operation": "started_default_priority",
+	}).Set(s.Cluster.Workload.Transactions.StartedDefaultPriority.Counter)
+
+	workloadTransactionsRoughness.With(prometheus.Labels{
+		"operation": "started_default_priority",
+	}).Set(s.Cluster.Workload.Transactions.StartedDefaultPriority.Roughness)
+
+	workloadTransactionsHZ.With(prometheus.Labels{
+		"operation": "started_immediate_priority",
+	}).Set(s.Cluster.Workload.Transactions.StartedImmediatePriority.Hz)
+
+	workloadTransactionsCounter.With(prometheus.Labels{
+		"operation": "started_immediate_priority",
+	}).Set(s.Cluster.Workload.Transactions.StartedImmediatePriority.Counter)
+
+	workloadTransactionsRoughness.With(prometheus.Labels{
+		"operation": "started_immediate_priority",
+	}).Set(s.Cluster.Workload.Transactions.StartedImmediatePriority.Roughness)
 }
 
 func registerWorkload(r *prometheus.Registry) {
-	r.MustRegister(workloadOperationHZ)
-	r.MustRegister(workloadOperationCounter)
-	r.MustRegister(workloadTransactionHZ)
-	r.MustRegister(workloadTransactionCounter)
+	r.MustRegister(workloadBytesHZ)
+	r.MustRegister(workloadBytesCounter)
+	r.MustRegister(workloadBytesRoughness)
+	r.MustRegister(workloadKeysHZ)
+	r.MustRegister(workloadKeysCounter)
+	r.MustRegister(workloadKeysRoughness)
+	r.MustRegister(workloadOperationsHZ)
+	r.MustRegister(workloadOperationsCounter)
+	r.MustRegister(workloadOperationsRoughness)
+	r.MustRegister(workloadTransactionsHZ)
+	r.MustRegister(workloadTransactionsCounter)
+	r.MustRegister(workloadTransactionsRoughness)
+
 	fmt.Println("registered workload metrics")
 }
